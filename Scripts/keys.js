06f0641ba63a925905afa27fab08d5b0eec3e493
michaelc.pittman <michaelc.pittman@gmail.com>
2017-07-18 17:18:34 -0400

    key file push

---
+++ Scripts/keys.js
@@ -0,0 +1,10 @@
+```JavaScript
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'VKQOBd7PNxZGPAXD2jLkK86x5',
+  consumer_secret: 'cRXXppKQsWauiDaN6uU0kGREzL9eN8AUiqSGZwIOzA1nMsPU9N',
+  access_token_key: '887310972578398210-nOrr1jdKHuycAEoI0mLBUHep0n7djID',
+  access_token_secret: 'DbkBSWhceiqmnbKfRcPEDIbAu0jc3lL40G7cXpDKeejNF',
+}
+```
