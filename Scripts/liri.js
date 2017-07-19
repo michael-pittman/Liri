55492aa47326d4e1c590bdbde4e88a82e87ff48e
michaelc.pittman <michaelc.pittman@gmail.com>
2017-07-19 17:49:20 -0400

    twitter npm added
    also code for switch and main run fuction

--- Scripts/liri.js
+++ Scripts/liri.js
@@ -1,7 +1,37 @@
 // Grabs the bands variables
 var keys = require("./keys.js");
 
-// Gets all of myBands bands from the bands file.
 var keyList = keys.twitterKeys;
 
-console.log(keyList.consumer_secret)
+var Twitter = require('twitter');
+ 
+var client = new Twitter(keyList);
+ 
+var getTweets = function(){ 
+    var params = {screen_name: 'mcxviiv'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+    console.log(tweets);
+    for(var i=0; i<tweets.length; i++) {
+        console.log(tweets[i].text);
+        console.log(tweets[i].created_at + "\n");
+    }
+    }
+    });
+}
+
+var pick = function(caseData, functionData) {
+    switch(caseData) {
+        case 'my-tweets' :
+                getTweets();
+                break;
+        default:
+        console.log("Liri doesn't know that, yet...")
+    }
+}
+
+var runThis = function(argOne, argTwo) {
+    pick(argOne, argTwo);
+}
+
+runThis(process.argv[2], process.argv[3]);
